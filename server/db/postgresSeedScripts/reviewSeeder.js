36e6dd34b1cc958318cbe04e5ec8ee193566c3b5
Steven Papalia <stevenapapalia@yahoo.com>
2020-04-07 12:20:01 -0700

    Changed headers, no major changes

--- server/db/postgresSeedScripts/reviewSeeder.js
+++ server/db/postgresSeedScripts/reviewSeeder.js
@@ -1,8 +1,8 @@
 const fake = require('faker');
 const fs = require('fs');
 
-const writeReviews = fs.createWriteStream('server/db/seededResults/reviewsData.csv');
-writeReviews.write('_review_id,id_User,date,rating,body,useful_count,cool_count,funny_count,check_ins\n', 'utf8');
+const writeReviews = fs.createWriteStream('server/db/seededResults/reviewsDataPG.csv');
+writeReviews.write('review_id,id_user,id_restaurants,date,rating,body,useful_count,cool_count,funny_count,check_ins,useful_vote,cool_vote,funny_vote\n', 'utf8');
 
 function writeTenMillionUsers(writer, encoding, callback) {
   let i = 14855680;
@@ -14,14 +14,14 @@ function writeTenMillionUsers(writer, encoding, callback) {
       id += 1;
       const id_user = Math.floor(Math.random() * 99999 + 1);
       const id_restaurants = Math.floor(Math.random() * 99999 + 1);
-      const date = fake.date.between('2000-01-10', '2020-01-21');
+      const date = JSON.stringify(fake.date.between('2000-01-10', '2020-01-21'));
       const rating = Math.floor(Math.random() * 4 + 1);
-      const body = fake.lorem.sentences(i % 20 !== 0 ? Math.floor(Math.random() * 3 + 3) : 20);
+      const body = fake.lorem.sentences(i % 20 !== 0 ? Math.floor(Math.random() * 2 + 4) : 20);
       const useful_count = Math.floor(Math.random() * 8 + 1);
       const cool_count = Math.floor(Math.random() * 5 + 1);
       const funny_count = Math.floor(Math.random() * 2 + 1);
       const check_ins = Math.floor(Math.random() * 3);
-      const data = `${id},${id_user},${id_restaurants},${date},${rating},${body},${useful_count},${cool_count},${funny_count},${check_ins},\n`;
+      const data = `${id},${id_user},${id_restaurants},${date},${rating},${body},${useful_count},${cool_count},${funny_count},${check_ins},0,0,0\n`;
       if (i === 0) {
         writer.write(data, encoding, callback);
       } else {
