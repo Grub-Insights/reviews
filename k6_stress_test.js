5d197b20810328196827db4711a77b9bff61e1ab
Steven Papalia <stevenapapalia@yahoo.com>
2020-04-10 18:44:47 -0700

    setup new-relic and k6!

---
+++ k6_stress_test.js
@@ -0,0 +1,49 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export let options = {
+  stages: [
+    { duration: '2m', target: 100 }, // below normal load
+    { duration: '2m', target: 200 }, // normal load
+    { duration: '2m', target: 300 }, // around the breaking point
+    { duration: '2m', target: 400 }, // beyond the breaking point
+    { duration: '2m', target: 0 }, // scale down. Recovery stage.
+  ],
+};
+
+export default function() {
+  const BASE_URL = 'http://localhost:5001'; // make sure this is not production
+  let responses = http.batch([
+    [
+      'GET',
+      `${BASE_URL}/restaurants/1/reviews`,
+      null,
+      { tags: { name: 'RestaurantReviews1' } },
+    ],
+    [
+      'GET',
+      `${BASE_URL}/restaurants/100001/reviews`,
+      null,
+      { tags: { name: 'RestaurantReviews100k' } },
+    ],
+    [
+      'GET',
+      `${BASE_URL}/restaurants/500001/reviews`,
+      null,
+      { tags: { name: 'RestaurantReviews500k' } },
+    ],
+    [
+      'GET',
+      `${BASE_URL}/restaurants/1000000/reviews`,
+      null,
+      { tags: { name: 'RestaurantReviews1mill' } },
+    ],
+  ]);
+  responses.forEach((res) => {
+    check(res, { 
+      'status was 200': r => r.status == 200,
+      'transaction time OK': r => r.timings.duration < 2000,
+    });
+  });
+  sleep(1);
+}
